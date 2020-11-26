05c178f89bd747841984e4c12e96275b5caee725
wangrongding <wangrongding@qq.com>
2020-11-26 15:17:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img
+            alt="Vue logo"
+            src="./assets/logo.png"
+        >
+        <main />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import main from './components/main.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        main,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+const resolve = (dir) => path.join(__dirname, dir);
+
+module.exports = {
+    lintOnSave: false,
+    // 关闭生产环境的sourcemap
+    productionSourceMap: false,
+    devServer: {
+        host: "0.0.0.0",
+        port: 9530,
+        disableHostCheck: true,
+        proxy: {
+            "/api": {
+                target: process.env.VUE_APP_BASE_URL,
+                changeOrigin: true,
+                ws: false,
+                pathRewrite: {
+                    "^/api": "/",
+                },
+            },
+        },
+    },
+    // 开启gzip压缩
+    configureWebpack: () => {
+        if (process.env.NODE_ENV === "production") {
+            return {
+                plugins: [
+                    new CompressionPlugin({
+                        test: /\.js$|\.html$|\.css/,
+                        threshold: 10240,
+                        deleteOriginalAssets: false,
+                    }),
+                ],
+            };
+        }
+    },
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set("@", resolve("src"))
+            .set("components", resolve("src/components"));
+        config.plugin("html").tap((args) => {
+            args[0].title = "多人协同";
+            return args;
+        });
+    },
+};
